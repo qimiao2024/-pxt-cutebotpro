02e7b24ca1538c34684343975ee103f2fb41c90b
qimiao2024 <168467483+qimiao2024@users.noreply.github.com>
2024-08-29 22:09:43 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    CutebotPro.angleRunning(CutebotProWheel.LeftWheel, 360, CutebotProAngleUnits.Angle)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
